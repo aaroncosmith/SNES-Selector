39c055d2eee4447d2b412c8177cd3a0b2dc6b487
Aaron Smith <aaroncosmith@gmail.com>
2020-04-16 09:32:18 -0400

    finished loading api data

--- src/components/Gamecard.jsx
+++ src/components/Gamecard.jsx
@@ -5,6 +5,9 @@ class Gamecard extends Component {
         id: "",
         name: "",
         rating: "",
+        screenshot: "",
+        released: "",
+        genre: ""
 
 
     }
@@ -17,19 +20,25 @@ class Gamecard extends Component {
             this.setState({
                 id: data.results[0].id,
                 name: data.results[0].name,
-                rating: data.results[0].rating
+                rating: data.results[0].rating,
+                screenshot: data.results[0].background_image,
+                released: data.results[0].released,
+                genre: data.results[0].genres[0].name
             })
         } catch (error) {
             console.log("Error is ", error.message)
         }
     }
     render() {
-        const { id, name, rating } = this.state;
+        const { id, name, rating, screenshot, released, genre } = this.state;
         return (
             <div>
                 <p>{id}</p>
                 <p>{name}</p>
                 <p>{rating}</p>
+                <p>Release date: {released} </p>
+                <p>Genre: {genre}</p>
+                <img src={screenshot} alt="Game Screenshot"></img>
             </div>
         );
     }
